5489f92d30c606839c0ebefcdc273d9b3e50ffb2
Rhythm Munshi <programmingwithrm@gmail.com>
2022-04-17 23:25:02 +0600

    This is one Of the most Populer Courier Services Website in Bangladesh

--- src/Components/Home/Map/Map.js
+++ src/Components/Home/Map/Map.js
@@ -15,7 +15,7 @@ const Map = () => {
         <div className="map-container">
         <div className="container">
             <div className="row">
-                <div data-aos="fade-right" data-aos-easing="ease-in-sine" className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
+                <div data-aos="fade-right" className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
                     <img id="map" className="img-fluid" src={mapImg} alt="Map" />
                 </div>
                 <div data-aos="fade-right" className="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 textArea align-middle" >
